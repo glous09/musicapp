be402c808d29a6b927fc0df64ae5f75d6a9e0316
Adalid <witch_ada@hotmail.com>
2018-04-20 14:58:46 -0500

    componentes playlist y song

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
-export default class playlist extends Component {
+export default class Playlist extends Component {
   render() {
     return (
       <div className="playlist">
-        { this.props.children }
+        <form>
+          <button type="button" className="btn btn-dark">+ Add List.</button>
+          <div className="form-group">
+            <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Name"/>
+            </div>
+            <div class="form-group">
+              <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="URL Image"/>
+            </div>
+              <button type="submit">add</button>
+        </form>
       </div>
     )
   }
-}
+};
+
--- src/components/song.js
+++ src/components/song.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
-export default class song extends Component {
+export default class Song extends Component {
   render() {
     return (
       <div className="song">
-        { this.props.children }
+        <div>
+          <h3 className="d-inline" >PlayList Name</h3>
+          <img src="" alt="image"/>
+        </div>
+          <div>
+            <form>
+              <div className="form-group">
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Song"/>
+                </div>
+                <div className="form-group">
+                  <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Artist"/>
+                </div>
+                  <div className="form-group">
+                    <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Album"/>
+                </div>
+                    <button type="submit">add</button>
+            </form>
+                </div>
       </div>
     )
   }
